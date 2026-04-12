2463c836455a94ac69d20dfd76fa7ff1b1447ecf
Uematus <vetakom@mail.ru>
2026-04-12 13:51:30 +0000

    Changes in mart.

--- db_schema/06_mart_tables.sql
+++ db_schema/06_mart_tables.sql
@@ -8,15 +8,7 @@ CREATE TABLE IF NOT EXISTS mart.daily_weather (
 
     -- Grain: 1 day × 1 city
     date                DATE        NOT NULL,   -- relates to PBI Calendar table
-    city_name           TEXT        NOT NULL,
-
-    -- City / Geography
-    latitude            FLOAT       NOT NULL,
-    longitude           FLOAT       NOT NULL,
-    is_capital          BOOLEAN,
-    climate_zone        TEXT,
-    country_name        TEXT        NOT NULL,
-    region              TEXT,
+    city_id             TEXT        NOT NULL,
 
     -- Weather description (human-readable WMO code)
     weather_description TEXT,
@@ -40,6 +32,7 @@ CREATE TABLE IF NOT EXISTS mart.daily_weather (
 
     -- Atmosphere
     humidity_avg        FLOAT,
+    pressure_avg        FLOAT,          -- mean sea-level pressure, hPa
 
     -- Day flags
     is_frost_day        BOOLEAN,
@@ -47,11 +40,11 @@ CREATE TABLE IF NOT EXISTS mart.daily_weather (
     is_rainy_day        BOOLEAN,
     is_snowy_day        BOOLEAN,
 
-    PRIMARY KEY (date, city_name)
+    PRIMARY KEY (date, city_id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_mart_daily_weather_date
     ON mart.daily_weather (date);
 
 CREATE INDEX IF NOT EXISTS idx_mart_daily_weather_city
-    ON mart.daily_weather (city_name);
+    ON mart.daily_weather (city_id);
--- weather/assets/mart/mart_daily_weather.sql
+++ weather/assets/mart/mart_daily_weather.sql
@@ -13,13 +13,7 @@ WHERE date BETWEEN '{{ start_date }}'::date AND '{{ end_date }}'::date;
 
 INSERT INTO mart.daily_weather (
     date,
-    city_name,
-    latitude,
-    longitude,
-    is_capital,
-    climate_zone,
-    country_name,
-    region,
+    city_id,
     weather_description,
     temp_avg,
     temp_min,
@@ -31,6 +25,7 @@ INSERT INTO mart.daily_weather (
     wind_speed_avg,
     wind_gusts_max,
     humidity_avg,
+    pressure_avg,
     is_frost_day,
     is_hot_day,
     is_rainy_day,
@@ -38,13 +33,7 @@ INSERT INTO mart.daily_weather (
 )
 SELECT
     cal.date,
-    ci.city_name,
-    ci.latitude,
-    ci.longitude,
-    ci.is_capital,
-    ci.climate_zone,
-    co.country_name,
-    co.region,
+    f.city_id,
     wc.description          AS weather_description,
     f.temp_avg,
     f.temp_min,
@@ -56,6 +45,7 @@ SELECT
     f.avg_wind_speed_10m    AS wind_speed_avg,
     f.max_wind_gusts        AS wind_gusts_max,
     f.avg_relative_humidity AS humidity_avg,
+    f.avg_pressure_msl      AS pressure_avg,
     f.is_frost_day,
     f.is_hot_day,
     f.is_rainy_day,
@@ -63,8 +53,6 @@ SELECT
 
 FROM core.fact_weather_daily f
 JOIN core.dim_calendar      cal ON cal.id  = f.date_id  -- needed only to resolve date value
-JOIN core.dim_cities        ci  ON ci.id   = f.city_id
-JOIN core.dim_countries     co  ON co.id   = ci.country_id
 LEFT JOIN core.dim_weather_code wc ON wc.id = f.dominant_weather_code_id
 
 WHERE cal.date BETWEEN '{{ start_date }}'::date AND '{{ end_date }}'::date;
